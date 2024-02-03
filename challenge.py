1c58a607b1d5740177b9dee1d2f3379e2156cdc2
Alexandre_Santos_2011 <elizias781220@gmail.com>
2024-02-02 21:56:09 -0300

    obtaining access token

---
+++ challenge.py
@@ -0,0 +1,55 @@
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = "1BxeqyHIt4vG6hPt9t9h9Ha24QCvul8UQh6GK_4u3HrA"
+SAMPLE_RANGE_NAME = "Class Data!A4:H27"
+
+
+def main():
+
+  creds = None
+
+  if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+  try:
+    service = build("sheets", "v4", credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = (
+        sheet.values()
+        .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
+        .execute()
+    )
+   # values = result.get("values", [])
+
+
+  except HttpError as err:
+    print(err)
+
+if __name__ == "__main__":
+  main()
+
